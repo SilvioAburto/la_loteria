58505738300a28b29ae32aa550ad6b50ea82aeaf
silvioaburto <sjortiz@ucdavis.edu>
2019-12-31 14:30:44 -0800

    fix rel path

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ class App extends Component {
       audio_gallo.play()
     }
 
-    let audio_dama = new Audio("/la_dama.mp3")
+    let audio_dama = new Audio("la_loteria/la_dama.mp3")
     const start_dama = () => {
       audio_dama.play()
     }
 
-    let audio_diablito = new Audio("/el_diablito.mp3")
+    let audio_diablito = new Audio("la_loteria/el_diablito.mp3")
     const start_diablito = () => {
       audio_diablito.play()
     }
