ca26a3a5839b8925c0ecd98445ec67bdddccaf00
silvioaburto <sjortiz@ucdavis.edu>
2020-01-02 09:32:13 -0800

    sound test; ;

--- src/App.js
+++ src/App.js
@@ -66,6 +66,116 @@ class App extends Component {
     };
 }
 
+render_sound(){
+  var music = new Audio("http://curtastic.com/nightmare.mp3")
+
+var chime = new Audio("http://curtastic.com/gold.wav")
+
+var nothing = new Audio("http://touchbasicapp.com/nothing.wav")
+
+var allAudio = []
+
+allAudio.push(music)
+
+allAudio.push(chime)
+
+
+
+var tapped = function() {
+
+
+// Play all audio files on the first tap and stop them immediately.
+
+if(allAudio) {
+
+for(var audio of allAudio) {
+
+  audio.play()
+
+  audio.pause()
+
+  audio.currentTime = 0
+
+}
+
+allAudio = null
+
+}
+
+
+
+// We should be able to play music delayed now (not during the tap event).
+
+//messagediv.innerHTML = "Music starts in 2 seconds..."
+
+setTimeout(function() {
+
+//messagediv.innerHTML = "Music playing. <button onclick='stop()'>Stop</button>"
+
+music.play()
+
+}, 2000)
+
+
+
+}
+
+document.body.addEventListener('touchstart', tapped, false)
+
+document.body.addEventListener('click', tapped, false)
+
+
+
+var stop = function() {
+
+music.pause()
+
+loop = null
+
+document.body.removeEventListener('touchstart', tapped, false)
+
+document.body.removeEventListener('click', tapped, false)
+
+}
+
+
+
+// Check if audio starts already unlocked by playing a blank wav.
+
+nothing.play().then(function() {
+
+//lockeddiv.innerHTML = "Audio started unlocked!"
+
+}).catch(function(){
+
+//lockeddiv.innerHTML = "Audio started locked :("
+
+})
+
+
+
+var loop = function() {
+
+// Try to play chimes whenever we want (not during user action).
+
+if(Math.random() < .01) {
+
+chime.play().then(function(){
+
+//	lockeddiv.innerHTML = "Audio is now unlocked!"
+
+})
+
+}
+
+setTimeout(loop, 16)
+
+}
+
+  loop()
+}
+
+
   getCardIndex(){
     return(this.state.card_index)
   }
@@ -142,11 +252,13 @@ class App extends Component {
             return (
               <div className="App">
                 <div className="title">La Loteria</div>
+                <button  className="play_button" onClick={() => this.render_sound()}>Play</button>
                 <h2>
                   {this.getCardIndex()}/{this.imageLength()}
                 </h2>
                 <div id = "img_id" className = "img_class"></div>
                 <div className = "cards_footer">
+
                   <button  className="start_button" onClick={() => this.change_card()}>START</button>
                   <button  className="pause_button" onClick={() => this.pauseTime()}>PAUSE</button>
                   <button  className="reset_button" onClick={() => this.resetCards()}>RESET</button>
